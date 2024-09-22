4ef59ed2b8f0222ea89a1e3f109c56f7c673df00
20mEmeraldSplash <yichenwaa@gmail.com>
2024-09-22 06:25:59 -0400

    Initial Commit

---
+++ iOSApp/iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppApp.swift
+//  iOSApp
+//
+//  Created by 汪猛男 on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
